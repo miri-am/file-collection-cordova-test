c542ee5590ca550f11a8524aabca085da42f6392
Dirk Volland <dv.junin@gmail.com>
2015-09-23 17:41:31 +0200

    added mobile-config.js

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  //id: 'ch.healthpilotapril', //PROD
+  id: 'some.id.com', //DEV
+  name: 'testingFileCol',
+  description: '',
+  author: 'Miri',
+  email: '',
+  website: '',
+  version: '0.0.1',
+  buildNumber: '100'
+});
+
+App.accessRule("*");//should be a little more tightly controlled
